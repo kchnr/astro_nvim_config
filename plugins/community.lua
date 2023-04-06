8ebf27ae2e34e0f101d3f35d828a1765d760cfc1
kchnr <jean.kirchner@gmail.com>
2023-04-06 20:12:14 -0300

    install some community packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,9 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.php" },
 }
