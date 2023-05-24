1d284f9eda3ddedd6c4186142650de11b9be54d7
kchnr <jean.kirchner@gmail.com>
2023-05-24 00:17:21 -0300

    ..

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,11 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.pack.go" },
-  -- { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.toml" },
-  -- { import = "astrocommunity.pack.prisma" },
-  -- { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.json" },
 }
